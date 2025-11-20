cb8a56ed585eef9926d9e25b88114e4f7fd223e8
Oskari Järvelin <contact@oskarijarvelin.fi>
2025-11-20 22:16:38 +0200

    refactor: Change createClient to an async function and retrieve cookieStore internally

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,7 +5,9 @@ import { cookies } from "next/headers";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export async function createClient() {
+  const cookieStore = await cookies();
+  
   return createServerClient(
     supabaseUrl!,
     supabaseKey!,
@@ -26,4 +28,4 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
       },
     },
   );
-};
+}
